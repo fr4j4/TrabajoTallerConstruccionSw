9c386d05e3bf8d4419e94e3448626207a5a41665
Francisco González <fgonz@outlook.com>
2016-06-17 02:50:25 -0400

    test

---
+++ test/test2.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test1.ui'
+#
+# Created: Fri Jun 17 02:49:40 2016
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.tableView = QtGui.QTableView(self.centralwidget)
+        self.tableView.setGeometry(QtCore.QRect(10, 60, 621, 331))
+        self.tableView.setObjectName(_fromUtf8("tableView"))
+        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 20, 113, 20))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.comboBox = QtGui.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(150, 20, 69, 22))
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.groupBox = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(240, 0, 221, 51))
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.radioButton = QtGui.QRadioButton(self.groupBox)
+        self.radioButton.setGeometry(QtCore.QRect(10, 20, 82, 17))
+        self.radioButton.setObjectName(_fromUtf8("radioButton"))
+        self.radioButton_2 = QtGui.QRadioButton(self.groupBox)
+        self.radioButton_2.setGeometry(QtCore.QRect(100, 20, 82, 16))
+        self.radioButton_2.setObjectName(_fromUtf8("radioButton_2"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(480, 30, 75, 23))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 400, 75, 23))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.pushButton_3 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(550, 400, 75, 23))
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.pushButton_4 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(460, 400, 75, 23))
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.groupBox.setTitle(_translate("MainWindow", "GroupBox", None))
+        self.radioButton.setText(_translate("MainWindow", "RadioButton", None))
+        self.radioButton_2.setText(_translate("MainWindow", "RadioButton", None))
+        self.pushButton.setText(_translate("MainWindow", "PushButton", None))
+        self.pushButton_2.setText(_translate("MainWindow", "PushButton", None))
+        self.pushButton_3.setText(_translate("MainWindow", "PushButton", None))
+        self.pushButton_4.setText(_translate("MainWindow", "PushButton", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
