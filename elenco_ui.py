012d2e4d9225d00132c49a499ada57cc1606800b
fr4j4 <fgonz@outlook.com>
2016-09-05 21:54:28 -0300

    armageddon2

--- elenco_ui.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ElencoDialog.ui'
-#
-# Created: Sun Sep  4 23:58:40 2016
-#      by: PyQt4 UI code generator 4.10.4
-#
-# WARNING! All changes made in this file will be lost!
-
